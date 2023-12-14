da0670e9aa1fcf80462750ad965c706f413d9c82
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-12-14 23:52:15 +0600

    project created

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to StayVista</h1>
+      {/*TODO: Categories Section */}
+      {/* TODO: Rooms section */}
+    </div>
+  )
+}
+
+export default Home
